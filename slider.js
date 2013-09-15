f9d37ff4fbce4ab6b230f0bda18a1f79e946e5d9
Hudkoff <alexus.web@gmail.com>
2013-09-15 20:53:22 +0300

    piu piu

--- slider.js
+++ slider.js
@@ -1,105 +1,218 @@
-var output = "";
+﻿/**
+ * inserts slider into specified container and makes everything
+ * @param  {string}  parameters.path        folder name or path "folder1/folder2"
+ * @param  {string}  parameters.images      images list separated with ","
+ * @param  {string}  parameters.placeholder id of element to which insert slider
+ * @param  {number}  parameters.width       width of container and images
+ * @param  {number}  parameters.height      height of container and images
+ * @param  {number}  parameters.direction   forward backward sliding (+1 | -1)
+ * @param  {number}  parameters.duration    interval
+ */
+function Slider(parameters) {
+	var self = this;
 
-function slider(parameters) {
-
-	///////////////////////// settings /////////////////////////
-	// path to file if specified, root - if not
-	var path = parameters.path? parameters.path + "/" : "";
-	// images parameter processing
+	// --------------------- PRIVATE ----------------------------
+	var path = parameters.path? parameters.path + "/" : ""; // root if empty
 	var imagesArray = parameters.images.replace(new RegExp(" ",'g'),"").split(","); // remove all empty spaces and make Array
-	// define block to whick insert slider
 	var placeholder = document.getElementById(parameters.placeholder);
-	// size
-	placeholder.className += " slideWrapper";
-	var width = parameters.width? parameters.width : 472;
-	var height = parameters.height?  parameters.height : 238;
-	placeholder.style.width = width + "px";
-	placeholder.style.height = height + "px";
-	// speed
-	var duration = parameters.duration? parameters.duration : "500";
-	// forward/backward
+	var width = parameters.width? parseInt(parameters.width) : 472; // some verification and magic should be there
+    var height = parameters.height?  parseInt(parameters.height) : 238;
+	var duration = parameters.duration? parameters.duration : "2000";
 	var direction = parameters.direction? parameters.direction : +1;
 
+	///////////////////////// styling /////////////////////////
+	placeholder.className += " slideWrapper"; // common class for css styles
+	placeholder.style.width = width + "px";
+
+
+	// --------------------- PUBLIC ----------------------------
+
+	/**
+	 * 1) generates HTML of Prev and Next links
+	 * 2) bind events on them
+	 */
+	this.generateNav = function () {
+		var prevNav = document.createElement("a");
+		prevNav.className = "prev";
+		prevNav.href = "#";
+		var nextNav = prevNav.cloneNode(true);
+		nextNav.className = "next";
+		// lets insert two links
+		placeholder.appendChild(prevNav);
+		placeholder.appendChild(nextNav);
+		prevNav.addEventListener('click', function(e) {
+			window.clearInterval(timer);
+			counter(-1);
+			addSlide(step, 0);
+			e.preventDefault();
+		});
+		nextNav.addEventListener('click', function(e) {
+			window.clearInterval(timer);
+			counter(+1);
+			addSlide(step, 0);
+			e.preventDefault();
+		});
+	};
+	this.generateNav();
+
+	/**
+	 * 1) generates HTML of miniatures
+	 * 2) bind events on them
+	 */
+	this.generatePlayPause = function () {
+		var playNav = document.createElement("a");
+		playNav.className = "play";
+		playNav.href = "#";
+		var pauseNav = playNav.cloneNode(true);
+		pauseNav.className = "pause";
+		// lets insert two links
+		placeholder.appendChild(playNav);
+		placeholder.appendChild(pauseNav);
+	};
+	this.generatePlayPause();
+
+	/**
+	 * generates HTML of slides wrapper
+	 */
+	this.generateSlidesWrapper = function () {
+		var el = document.createElement("div");
+		el.className = "slidesContainer";
+		placeholder.appendChild(el);
+		el.style.width = width + "px";
+		el.style.height = height + "px";
+	};
+	this.generateSlidesWrapper();
+	
+	/**
+	 * 1) generates HTML of miniatures
+	 * 2) bind events on them
+	 */
+	this.generateThumbnails = function () {
+		var el = document.createElement("div"); // placeholder.
+		el.className = "thumbnails";
+		placeholder.appendChild(el);
+		el.style.width = width + "px";
+		
+		for (var i = 0; i < imagesArray.length; i++) {
+			el.appendChild( makeImgTag(imagesArray[i], "tmbn") );
+		}
+	};
+	this.generateThumbnails();
+
+	/**
+	 * moves last of slides from [fromPos] to [toPos] and removes previous slide after that
+	 * @param  {HTMLElement} 	opts.target		moved element
+	 * @param  {Boolean} 	opts.removeAfterMoving		moved element
+	 * @param  {number} 		opts.shift
+	 */
+	this.animate = function (opts) {
+		var target = opts.target;
+		var fromPos = parseInt(target.style.left);			// 472
+		var toPos = fromPos + opts.shift;					// -472
+
+		var slidePos = fromPos;
+
+		window.setTimeout(function run() {
+			if (opts.shift < 0 && slidePos > toPos ) {		// moving to left && not arrived yet
+				slidePos -= Math.round(width/20);
+				target.style.left = slidePos + "px";
+				window.setTimeout(run, 10);
+			} else if (opts.shift > 0 && slidePos < toPos ) {
+				slidePos += Math.round(width/20);
+				target.style.left = slidePos + "px";
+				window.setTimeout(run, 10);
+			} else {
+				if (opts.removeAfterMoving) {
+					target.parentNode.removeChild(target);
+				}
+				target.style.left = toPos + "px";			// re-align if we have rounding inaccuracy
+			}
+		}, 10 );
+	};
 
 
 
-	var children = placeholder.childNodes; // all slides
 
+
+
+	///////////////////////// start auto rotating /////////////////////////
+	var newSlide = addSlide(0, 0);
+	var step = 0;
 	var numberOfSlides = imagesArray.length;
 
-	///////////////////////// processing /////////////////////////
-	// for each image in array make html tag
-	//for (var i = 0; i < numberOfSlides; i++) {
-	//	output += makeImgTag(imagesArray[i]);
-	//}
-
-	///////////////////////// rendering /////////////////////////
-	//placeholder.innerHTML = output;
-
-	///////////////////////// rotating /////////////////////////
-	showSlide(0);
-	var timer = setInterval(function() { letsCount(direction) }, duration); // +1 or -1 for now only!
+	var timer = window.setInterval(function() {
 
 
-	var step = 0;
-	function letsCount(direction) {
+		self.animate({                                                           // moving CURRENT slide
+			target: newSlide,
+			shift: direction > 0 ? -width : width,
+			removeAfterMoving: true                                                // and delete CURRENT slide
+		});
+		counter(direction);                                                        // step++
+		newSlide = addSlide(step, direction > 0 ? width : -width);                 // add NEXT slide
+		self.animate({                                                           // moving NEXT slide
+			target: newSlide,
+			shift: direction > 0 ? -width : width,
+			removeAfterMoving: false
+		});
 
-		//hideSlide(step); // REMOVE
+
+	}, duration); // +1 or -1 for now only!
+
+
+	//function
+
+	/**
+	 * counts from 1 to (numberOfSlides-1) [or backward, depends on direction] and gives slide number to addSlide() function
+	 * @param  {number} direction	(+1 | -1)
+	 *
+	 * @return {number} step		N of current slide
+	 */
+	function counter(direction) {
 		step = step + direction;
-
-		if ((direction > 0) && (step > numberOfSlides - 1)) {
+		if (step > numberOfSlides - 1) {
 			step = 0;
-		} else if ((direction < 0) && (step < 0)) {
+		}
+		if (step < 0) {
 			step = numberOfSlides - 1;
 		}
+		return step;
+	} 
 
-		showSlide(step); // ANIMATE
 
-	} // END letsCount()
+	/**
+	 * appends image tag to container and starts animate
+	 * @param {number}	whatSlideToShow  	array of images key
+	 * @param {number}	initialPosition
+	 *
+	 * @return {HTMLElement} newSlide
+	 */
+	function addSlide(whatSlideToShow, initialPosition) {
+		
+		newSlide = makeImgTag(imagesArray[whatSlideToShow], "slide");
+		placeholder.getElementsByClassName("slidesContainer")[0].appendChild(newSlide); // append <img> with next or previous jpg
+		newSlide.style.left = initialPosition + "px";
 
-	function makeImgTag(imgUrl) {
-		var imgTag = '<img src="' + path + imgUrl + '" class="slide" alt="" />';
+		return newSlide;
+	}
+
+
+	/**
+	 * converts image name to image tag
+	 * @param imgFilename {string}	imgFilename  single image name from array
+	 * @param className {string}	class that applies to image
+	 *
+	 * @returns {HTMLElement}		full <img> tag
+	 */
+	function makeImgTag(imgFilename, className) {
+		var imgTag = document.createElement("img");
+		imgTag.className = className;
+		imgTag.href = "#";
+		imgTag.src = path + imgFilename;
 		return imgTag;
-	} // END makeImgTag()
+	}
 
 
-	//function hideSlide(whatSlideToHide) {
-	//	setTimeout(function() {
-	//		placeholder.removeChild(placeholder.firstChild);
-	//	}, duration);
-	//} // END showSlide()
-
-	function showSlide(whatSlideToShow) {
-		placeholder.innerHTML += makeImgTag(imagesArray[whatSlideToShow]); // append <img> with next or previous jpg
-		animation((direction > 0)? width : -width, 0);
-	} // END showSlide()
-
-	var sliding = false;
-	var slidePos = 0;
-
-	function animation(fromPos, toPos) {
-
-		curSlide = placeholder.lastChild;
-		curSlide.style.left = fromPos + "px";
-		slidePos = fromPos;
-
-		sliding = clearInterval(sliding);
-     
-		sliding = setInterval(function() {
-			if (fromPos >= 0 && slidePos > toPos ) {
-				slidePos -= width/20;
-				curSlide.style.left = slidePos + "px";
-			} else if (fromPos <= 0 && slidePos < toPos ) {
-				slidePos += 20;
-				curSlide.style.left = slidePos + "px";
-			} else {
-				sliding = clearInterval(sliding);
-				if (curSlide.previousElementSibling) placeholder.removeChild(curSlide.previousElementSibling);  // WTF? why function don't stop after clearInterval - we need remove 1st <img> once manualy
-				curSlide.style.left = toPos + "px";
-        	}
-	    }, 10 );
-
-	} // END animation()
 
 
 } // END slider()
